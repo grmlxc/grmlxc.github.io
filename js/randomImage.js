04b48b6678cd8c5cbd6ada2572d6bd2638cd2ea1
Shita <shita0829@gmail.com>
2025-02-11 10:14:38 +0800

    test07

---
+++ js/randomImage.js
@@ -0,0 +1,22 @@
+// JavaScript Document
+
+// Array of image URLs
+const images = [
+    'images/bg1.jpg',
+    'images/bg2.jpg',
+    'images/bg3.jpg',
+    'images/bg4.jpg',
+	'images/bg5.jpg',
+	'images/bg6.jpg',
+	'images/bg7.jpg'
+];
+
+// Function to set a random background image
+function setRandomBackground() {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    const selectedImage = images[randomIndex];
+    document.getElementById('custom-header').style.backgroundImage = `url('${selectedImage}')`;
+}
+
+// Set a random background image on page load
+window.onload = setRandomBackground;
