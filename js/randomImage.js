046e04e797c223f109c0f97d8f49be2fb050184b
grmlxc <grmcuhk.micromodule@gmail.com>
2025-03-03 07:57:40 +0800

    test08

--- js/randomImage.js
+++ js/randomImage.js
@@ -8,7 +8,12 @@ const images = [
     'images/bg4.jpg',
 	'images/bg5.jpg',
 	'images/bg6.jpg',
-	'images/bg7.jpg'
+	'images/bg7.jpg',
+	'images/bg8.jpg',
+	'images/bg9.jpg',
+	'images/bg10.jpg',
+	'images/bg11.jpg',
+	'images/bg12.jpg',
 ];
 
 // Function to set a random background image
